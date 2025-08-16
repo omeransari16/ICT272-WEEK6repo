5be4383976d918fc0b95ae7808cb316beed471f3
omeransari16 <140717706+omeransari16@users.noreply.github.com>
2025-08-17 05:23:02 +1000

    Change 2
    Added the age object of customer class. Updated the constructor and print statements and customer data.

--- CustomerConsoleAppTutorial4/CustomerConsoleAppTutorial4/Program.cs
+++ CustomerConsoleAppTutorial4/CustomerConsoleAppTutorial4/Program.cs
@@ -13,12 +13,15 @@ namespace CustomerConsoleAppTutorial4
         public string Name { get; set; }
         public string Email { get; set; }
 
+        public int Age { get; set; }
+
         // Constructor to initialize the properties
-        public Customer(int id, string name, string email)
+        public Customer(int id, string name, string email, int age)
         {
             Id = id;
             Name = name;
             Email = email;
+            Age = age;
         }
     }
 
@@ -27,15 +30,17 @@ namespace CustomerConsoleAppTutorial4
         static void Main()
         {
             // Create two Customer objects with different values
-            Customer Cust1 = new Customer(1, "John Doe", "john.doe@example.com");
-            Customer Cust2 = new Customer(2, "Jane Smith", "jane.smith@example.com");
-            Customer Cust3 = new Customer(3, "Alex Kim", "alex.kim@example.com");
+            Customer Cust1 = new Customer(1, "John Doe", "john.doe@example.com", 12);
+            Customer Cust2 = new Customer(2, "Jane Smith", "jane.smith@example.com", 8);
+            Customer Cust3 = new Customer(3, "Alex Kim", "alex.kim@example.com", 9);
+
 
             // Print the details of the first customer using Console.WriteLine
             Console.WriteLine("Customer 1 Information:");
             Console.WriteLine("ID: " + Cust1.Id);
             Console.WriteLine("Name: " + Cust1.Name);
             Console.WriteLine("Email: " + Cust1.Email);
+            Console.WriteLine("Age: " + Cust1.Age);
             Console.WriteLine(); // Blank line for separation
 
             // Print the details of the second customer using Console.WriteLine
@@ -43,6 +48,7 @@ namespace CustomerConsoleAppTutorial4
             Console.WriteLine("ID: " + Cust2.Id);
             Console.WriteLine("Name: " + Cust2.Name);
             Console.WriteLine("Email: " + Cust2.Email);
+            Console.WriteLine("Age: " + Cust2.Age);
             Console.WriteLine(); // Blank line for separation
 
             // Print the details of the second customer using Console.WriteLine
@@ -50,6 +56,7 @@ namespace CustomerConsoleAppTutorial4
             Console.WriteLine("ID: " + Cust3.Id);
             Console.WriteLine("Name: " + Cust3.Name);
             Console.WriteLine("Email: " + Cust3.Email);
+            Console.WriteLine("Age: " + Cust3.Age);
         }
     }
 }
